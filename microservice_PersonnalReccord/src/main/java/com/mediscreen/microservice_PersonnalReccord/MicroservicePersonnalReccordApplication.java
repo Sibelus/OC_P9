62eb58b2dcf4795e7c31250ba4f6bf818116c238
4H0 <ggaetan@proton.me>
2023-06-03 11:36:13 +0200

    First commit

---
+++ microservice_PersonnalReccord/src/main/java/com/mediscreen/microservice_PersonnalReccord/MicroservicePersonnalReccordApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.microservice_PersonnalReccord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePersonnalReccordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicePersonnalReccordApplication.class, args);
+	}
+
+}
---
+++ microservice_PersonnalReccord/src/test/java/com/mediscreen/microservice_PersonnalReccord/MicroservicePersonnalReccordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.microservice_PersonnalReccord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePersonnalReccordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
