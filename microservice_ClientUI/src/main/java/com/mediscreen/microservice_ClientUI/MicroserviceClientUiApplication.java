68b82b82068acc27aaa3f1db0a33c217e8328313
4H0 <ggaetan@proton.me>
2023-06-05 09:20:17 +0200

    Add microservice ClientUI

---
+++ microservice_ClientUI/src/main/java/com/mediscreen/microservice_ClientUI/MicroserviceClientUiApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.microservice_ClientUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceClientUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceClientUiApplication.class, args);
+	}
+
+}
---
+++ microservice_ClientUI/src/test/java/com/mediscreen/microservice_ClientUI/MicroserviceClientUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.microservice_ClientUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceClientUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
