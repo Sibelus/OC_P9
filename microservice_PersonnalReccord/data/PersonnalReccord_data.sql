8acf14182adec368e19dfe8162fc18c83570072b
4H0 <ggaetan@proton.me>
2023-06-05 09:12:56 +0200

    Create database structure

---
+++ microservice_PersonnalReccord/data/PersonnalReccord_data.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS `mediscreen`;
+CREATE DATABASE mediscreen;
+USE mediscreen;
+
+-- TABLE CREATION
+DROP TABLE IF EXISTS `personnalreccord`;
+
+CREATE TABLE personnalreccord (
+`id` INTEGER NOT NULL AUTO_INCREMENT,
+`firstname` VARCHAR(100) NOT NULL,
+`lastname` VARCHAR(100) NOT NULL,
+`birthdate` DATE NOT NULL,
+`sex` VARCHAR(1) NOT NULL,
+`address` VARCHAR(100) NOT NULL,
+`phone` VARCHAR(100) NOT NULL,
+PRIMARY KEY (`id`)
+);
+
+
+-- DATA INSERTION
+INSERT INTO `personnalreccord` (`id`, `firstname`, `lastname`, `birthdate`, `sex`, `address`, `phone`) VALUES
+(1, 'Florence', 'Aignement', '1966-12-31', 'F', 'Secret service headquarter', '100-222-3333'),
+(2, 'Steff', 'Bihaille', '1983-03-27', 'F', 'Secret service headquarter', '100-222-3333');
