f5fa86826602dd69036f014530a17d5caab71290
4H0 <ggaetan@proton.me>
2023-06-07 09:48:50 +0200

    Create PersonalRecordNotFoundException

---
+++ microservice_ClientUI/src/main/java/com/mediscreen/microservice_ClientUI/exception/PersonalRecordNotFoundException.java
@@ -0,0 +1,11 @@
+package com.mediscreen.microservice_ClientUI.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class PersonalRecordNotFoundException extends RuntimeException {
+    public PersonalRecordNotFoundException(String message) {
+        super(message);
+    }
+}
