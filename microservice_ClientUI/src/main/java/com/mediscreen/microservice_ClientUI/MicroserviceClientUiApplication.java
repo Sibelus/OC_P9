32bc07e030315cd097acb47f32d03e96345a2366
4H0 <ggaetan@proton.me>
2023-06-07 09:51:56 +0200

    Enable FeignClients for MicroserviceClientUIApplication

--- microservice_ClientUI/src/main/java/com/mediscreen/microservice_ClientUI/MicroserviceClientUiApplication.java
+++ microservice_ClientUI/src/main/java/com/mediscreen/microservice_ClientUI/MicroserviceClientUiApplication.java
@@ -2,7 +2,9 @@ package com.mediscreen.microservice_ClientUI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients("com.mediscreen.microservice_ClientUI")
 @SpringBootApplication
 public class MicroserviceClientUiApplication {
 
