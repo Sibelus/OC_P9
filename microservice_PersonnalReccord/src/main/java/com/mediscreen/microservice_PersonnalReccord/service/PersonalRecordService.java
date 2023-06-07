0a9ce1fc9967f120614074d1e8e1da617f21cf4b
4H0 <ggaetan@proton.me>
2023-06-07 09:55:11 +0200

    Add logging to get more info

--- microservice_PersonnalReccord/src/main/java/com/mediscreen/microservice_PersonnalReccord/service/PersonalRecordService.java
+++ microservice_PersonnalReccord/src/main/java/com/mediscreen/microservice_PersonnalReccord/service/PersonalRecordService.java
@@ -3,6 +3,8 @@ package com.mediscreen.microservice_PersonnalReccord.service;
 
 import com.mediscreen.microservice_PersonnalReccord.model.PersonalRecord;
 import com.mediscreen.microservice_PersonnalReccord.repository.PersonalRecordRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,32 +16,38 @@ public class PersonalRecordService implements IPersonalRecordService {
 
     @Autowired
     PersonalRecordRepository personalRecordRepository;
+    Logger logger = LoggerFactory.getLogger(PersonalRecordService.class);
 
 
     @Override
     public List<PersonalRecord> getPatientList() {
+        logger.debug("Get all personal records");
         List<PersonalRecord> allPatient = personalRecordRepository.findAll();
         return allPatient;
     }
 
     @Override
     public Optional<PersonalRecord> getPatientInfos(int id) {
+        logger.debug("Get personal record id: {}", id);
         Optional<PersonalRecord> patientInfo = personalRecordRepository.findById(id);
         return patientInfo;
     }
 
     @Override
     public void addPatientInfo(PersonalRecord personalRecord) {
+        logger.debug("Create new personal record to database, {}", personalRecord.toString());
         personalRecordRepository.save(personalRecord);
     }
 
     @Override
     public void updatePatientInfo(PersonalRecord personalRecord) {
+        logger.debug("Update personal record to database, {}", personalRecord.toString());
         personalRecordRepository.save(personalRecord);
     }
 
     @Override
     public void deletePatientInfo(PersonalRecord personalRecord) {
+        logger.debug("Delete personal record from database, {}", personalRecord.toString());
         personalRecordRepository.deleteById(personalRecord.getId());
     }
 }
