5b58c9ce218dcb9158c26bf22893ffdef80efed3
4H0 <ggaetan@proton.me>
2023-06-07 09:54:34 +0200

    Refactor create, update & delete methods

--- microservice_PersonnalReccord/src/main/java/com/mediscreen/microservice_PersonnalReccord/controller/PersonalRecordController.java
+++ microservice_PersonnalReccord/src/main/java/com/mediscreen/microservice_PersonnalReccord/controller/PersonalRecordController.java
@@ -3,11 +3,12 @@ package com.mediscreen.microservice_PersonnalReccord.controller;
 import com.mediscreen.microservice_PersonnalReccord.exceptions.PersonalRecordNotFoundException;
 import com.mediscreen.microservice_PersonnalReccord.model.PersonalRecord;
 import com.mediscreen.microservice_PersonnalReccord.service.IPersonalRecordService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +18,7 @@ public class PersonalRecordController {
 
     @Autowired
     IPersonalRecordService iPersonalRecordService;
+    Logger logger = LoggerFactory.getLogger(PersonalRecordController.class);
 
 
     // GetMapping methods
@@ -29,6 +31,7 @@ public class PersonalRecordController {
 
     @GetMapping("/personalRecord/{id}")
     public Optional<PersonalRecord> getPatientInfo(@PathVariable int id) {
+        logger.info("GET_personalRecord -> Check if personal record id: {} exists", id);
         Optional<PersonalRecord> patientInfo = iPersonalRecordService.getPatientInfos(id);
         if (!patientInfo.isPresent()) throw new PersonalRecordNotFoundException("There is no data for the id: " + id);
         return patientInfo;
@@ -36,31 +39,36 @@ public class PersonalRecordController {
 
     @GetMapping("/personalRecord/update/{id}")
     public Optional<PersonalRecord> updatePatientInfo(@PathVariable int id) {
+        logger.info("UPDATE_personalRecord -> Check if personal record id: {} exists", id);
         Optional<PersonalRecord> patientInfo = iPersonalRecordService.getPatientInfos(id);
         if (!patientInfo.isPresent()) throw new PersonalRecordNotFoundException("There is no data for the id: " + id);
         return patientInfo;
     }
 
-
-    // PostMapping methods
-    @PostMapping("/personalRecord/add")
-    public void createPatientInfo_submit(PersonalRecord personalRecord) {
-        iPersonalRecordService.addPatientInfo(personalRecord);
-    }
-
-    @PostMapping("/personalRecord/update/{id}")
-    public void updatePatientInfo_Submit(@PathVariable int id, PersonalRecord personalRecord) {
-        personalRecord.setId(id);
-        iPersonalRecordService.updatePatientInfo(personalRecord);
-    }
-
-    @PostMapping("/personalRecord/delete/{id}")
-    public void deletePatientInfo_submit(@PathVariable int id) {
+    @GetMapping("/personalRecord/delete/{id}")
+    public void deletePatientInfo(@PathVariable int id) {
+        logger.info("DELETE_personalRecord -> Check if personal record id: {} exists", id);
         Optional<PersonalRecord> personalRecord = iPersonalRecordService.getPatientInfos(id);
         if (personalRecord.isPresent()) {
             iPersonalRecordService.deletePatientInfo(personalRecord.get());
         } else {
             throw new PersonalRecordNotFoundException("Personal record id: " + id + " doesn't exist");
         }
+    }
+
+
+    // PostMapping methods
+    @PostMapping("/personalRecord/validate")
+    public ResponseEntity<PersonalRecord> createPatientInfo_submit(@RequestBody PersonalRecord personalRecord) {
+        logger.debug("Create request body contains, {}", personalRecord.toString());
+        iPersonalRecordService.addPatientInfo(personalRecord);
+        return new ResponseEntity<PersonalRecord>(personalRecord, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/personalRecord/update")
+    public ResponseEntity<PersonalRecord> updatePatientInfo_Submit(@RequestBody PersonalRecord personalRecord) {
+        logger.debug("Update request body contains, {}", personalRecord.toString());
+        iPersonalRecordService.updatePatientInfo(personalRecord);
+        return new ResponseEntity<PersonalRecord>(personalRecord, HttpStatus.OK);
     }
 }
