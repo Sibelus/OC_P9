4134930faa9fcf69ecd315fbaebf90c03bf66c6e
4H0 <ggaetan@proton.me>
2023-06-09 16:57:42 +0200

    Add microservice_PractitionerNote

---
+++ microservice_PractitionerNote/src/main/java/com/mediscreen/microservice_PractitionerNote/MicroservicePractitionerNoteApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.microservice_PractitionerNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePractitionerNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicePractitionerNoteApplication.class, args);
+	}
+
+}
---
+++ microservice_PractitionerNote/src/test/java/com/mediscreen/microservice_PractitionerNote/MicroservicePractitionerNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.microservice_PractitionerNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePractitionerNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
