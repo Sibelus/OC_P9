b89d8531939f07445b629006e9cf334a5bb7c2e5
4H0 <ggaetan@proton.me>
2023-06-15 11:25:28 +0200

    Add microservice-diabetesReport module

---
+++ microservice_DiabetesReport/src/main/java/com/mediscreen/microservice_DiabetReport/MicroserviceDiabetesReportApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.microservice_DiabetReport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDiabetesReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDiabetesReportApplication.class, args);
+	}
+
+}
---
+++ microservice_DiabetesReport/src/test/java/com/mediscreen/microservice_DiabetReport/MicroserviceDiabetesReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.microservice_DiabetReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDiabetesReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
