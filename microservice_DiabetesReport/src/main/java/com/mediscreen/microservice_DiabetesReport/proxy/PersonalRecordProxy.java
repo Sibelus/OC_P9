b4b733bc5e98ab97c56473a048069bceb49d0a84
4H0 <ggaetan@proton.me>
2023-06-17 17:10:10 +0200

    Add getPatientList() into PersonalRecordProxy

--- microservice_DiabetesReport/src/main/java/com/mediscreen/microservice_DiabetesReport/proxy/PersonalRecordProxy.java
+++ microservice_DiabetesReport/src/main/java/com/mediscreen/microservice_DiabetesReport/proxy/PersonalRecordProxy.java
@@ -5,12 +5,16 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
 import java.util.Optional;
 
 @FeignClient(name = "microservice-PersonalRecord", url = "localhost:8081")
 public interface PersonalRecordProxy {
 
     // getMapping methods
+    @GetMapping("/personalRecord/list")
+    List<PersonalRecordBean> getPatientList();
+
     @GetMapping("/personalRecord/{id}")
     public Optional<PersonalRecordBean> getPatientInfo(@PathVariable int id);
 }
